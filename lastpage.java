207f205bd1dadbfe00ace0730667c2c52fcd93b0
shekharchaudhary <shekharchaudhary@users.noreply.github.com>
2016-12-18 20:20:15 -0800

    Update lastpage.java

--- lastpage.java
+++ lastpage.java
@@ -1,22 +1,31 @@
-package game1;
+package servlets;
 
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class lastpage {
 
+
+public class LastPage {
+	private JLabel lable;
+	static FrameClass frame1;
+	static highScore hiScore;
+	static int Height;
+	static int Width;
+	static highScore HIscore;
 	public class Action {
 
 	}
 
-	private JFrame frame;
+	private JFrame frame11;
 
 	/**
 	 * Launch the application.
@@ -25,8 +34,8 @@ public class lastpage {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					lastpage window = new lastpage();
-					window.frame.setVisible(true);
+					LastPage window = new LastPage();
+					window.frame1.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -37,7 +46,7 @@ public class lastpage {
 	/**
 	 * Create the application.
 	 */
-	public lastpage() {
+	public LastPage() {
 		initialize();
 	}
 
@@ -46,69 +55,85 @@ public class lastpage {
 	 */
 	private void initialize() {
 
-		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.RED);
-		frame.setBounds(100, 100, 800, 800);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame1 = new FrameClass();
+		Height = frame1.getHeight();
+		Width =frame1.getWidth();
+		frame1.getContentPane().setForeground(new Color(0, 0, 205));
+		frame1.getContentPane().setFont(new Font("Lucida Grande", Font.BOLD, 50));
+		frame1.getContentPane().setBackground(Color.RED);
+		//frame.setBackground(Color.RED);
+		frame1.getContentPane().setLayout(null);
+		
+		// High Score
+		String HIscore ="";
+		try {
+			HIscore = highScore.updateHiScore(0).toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JLabel txtHighScore = new JLabel();
+		txtHighScore.setText("Your High Score: "+HIscore);
+		txtHighScore.setForeground(Color.YELLOW);
+		txtHighScore.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+		txtHighScore.setBounds(Width/2-150, Height/2-200, 380, 50);
+		frame1.getContentPane().add(txtHighScore);
+		//txtHighScore.setColumns(10);
 
-		JTextField txtHighScore = new JTextField();
-		txtHighScore.setText("HIGH SCORE");
-		txtHighScore.setFont(new Font("Lucida Grande", Font.BOLD, 14));
-		txtHighScore.setBounds(293, 201, 234, 39);
-		frame.getContentPane().add(txtHighScore);
-		txtHighScore.setColumns(10);
-
-		JTextField txtYourScore = new JTextField();
-		txtYourScore.setFont(new Font("Lucida Grande", Font.BOLD, 13));
-		txtYourScore.setText("YOUR SCORE");
+		JLabel txtYourScore = new JLabel();
+		txtYourScore.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+		txtYourScore.setText("Your Score:  ");
+		txtYourScore.setForeground(Color.YELLOW);
 		txtYourScore.setForeground(new Color(0, 0, 128));
-		txtYourScore.setBounds(293, 275, 234, 39);
-		frame.getContentPane().add(txtYourScore);
-		txtYourScore.setColumns(10);
+		txtYourScore.setBounds(Width/2-150, Height/2-250, 380, 50);
+		frame1.getContentPane().add(txtYourScore);
+		//txtYourScore.setColumns(10);
 
-		JButton btnNewButton = new JButton("START");
-		btnNewButton.setFont(new Font("Lucida Grande", Font.BOLD, 14));
-		btnNewButton.setBounds(354, 410, 117, 61);
-		frame.getContentPane().add(btnNewButton);
+		JButton btnNewButton = new JButton("REPLAY");
+		btnNewButton.setForeground(Color.GREEN);
+		btnNewButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+		btnNewButton.setBounds(Width/2-60, Height/2-100, 120, 50);
+		frame1.getContentPane().add(btnNewButton);
 		btnNewButton.addActionListener(new CustomActionListener());
 
 		JButton btnNewButton_1 = new JButton("EXIT");
-		btnNewButton_1.setFont(new Font("Lucida Grande", Font.BOLD, 14));
-		btnNewButton_1.setBounds(354, 563, 117, 61);
-		frame.getContentPane().add(btnNewButton_1);
-		frame.setResizable(false);
-		
-		btnNewButton_1.addActionListener(new action());
+		btnNewButton_1.setForeground(Color.GREEN);
+		btnNewButton_1.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+		btnNewButton_1.setBounds(Width/2-60, Height/2+10, 120, 50);
+		frame1.getContentPane().add(btnNewButton_1);
+		//frame1.setResizable(false);
+
+		btnNewButton_1.addActionListener(new EndButtonAction());
 
 	}
 
 	class CustomActionListener implements ActionListener {
-		public void actionPerformed1(ActionEvent e) {
-
-		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Start here
+			frame1.setVisible(false);
 			System.out.println("hello");
-			Activity n = new Activity();
+			// new Activity();
+			Activity a = new Activity();
+			a.ConnectActivity();
+
 		}
 
 	}
-	class action implements ActionListener {
-		public void actionPerformed1(ActionEvent e) {
 
-		}
-
+	class EndButtonAction implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO exit here
-		System.exit(0);
+			System.exit(0);
 		}
 
 	}
-	
-	
-	
+
+	public void ConnectLastpage() {
+		LastPage lastPage = new LastPage();
+		lastPage.frame1.setVisible(true);
+	}
+
 }
